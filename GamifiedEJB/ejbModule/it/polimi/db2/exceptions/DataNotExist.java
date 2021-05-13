91975fdb1cac32f604649d9d1b0cb98144d2c913
hezhitao <zhita.he@mail.polimi.it>
2021-05-13 13:09:07 +0800

    2222

--- GamifiedEJB/ejbModule/it/polimi/db2/exceptions/DataNotExist.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.db2.exceptions;
-
-public class DataNotExist extends Exception{
-    private static final long serialVersionUID = 1L;
-
-    public DataNotExist(String message) {
-        super(message);
-    }
-}
