483b824d7943a35d67d88650538da100210a27c4
Xiaoyu Luo <xy.law@outlook.com>
2021-05-19 09:24:04 +0200

    FIX Inspection BUG

---
+++ GamifiedWeb/src/GoToInspectionSelectPage.java
@@ -0,0 +1,137 @@
+import it.polimi.db2.entities.Product;
+import it.polimi.db2.entities.Questionnaire;
+import it.polimi.db2.services.ProductService;
+import it.polimi.db2.services.UserService;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.ejb.EJB;
+import javax.persistence.PersistenceException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/InspectionSelectPage")
+public class GoToInspectionSelectPage extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+
+    @EJB(name = "it.polimi.db2.services/UserService")
+    private UserService userService;
+
+    @EJB(name = "it.polimi.db2.services/ProductService")
+    private ProductService productService;
+
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Redirect to the Home page and add missions to the parameters
+        String pathContext = getServletContext().getContextPath();
+        HttpSession session = request.getSession();
+        if (session.isNew() || session.getAttribute("administrator") == null) {
+            response.sendRedirect(pathContext+ "/AdminIndex.html");
+            return;
+        }
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+        if (session.getAttribute("errorMsg") != null){
+            ctx.setVariable("errorMsg", session.getAttribute("errorMsg"));
+            request.getSession().removeAttribute("errorMsg");
+
+        }
+            String path = "/WEB-INF/InspectionSelectPage.html";
+
+
+        templateEngine.process(path, ctx, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        // If admin user is not logged in (not present in session) redirect to the login
+        String pathContext = getServletContext().getContextPath();
+        HttpSession session = request.getSession();
+        if (session.isNew() || session.getAttribute("administrator") == null) {
+            response.sendRedirect(pathContext+ "/AdminIndex.html");
+            return;
+        }
+
+        String strDate;
+        try {
+            strDate = StringEscapeUtils.escapeJava(request.getParameter("Date"));
+            if (strDate == null) {
+                throw new Exception("No empty filed!");
+            }
+        }catch (Exception e){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+            return;
+        }
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse(strDate, fmt);
+        Product product = new Product();
+        product = productService.getProductByDate(date);
+
+
+        if(product == null){
+            request.getSession().setAttribute("errorMsg", "Wrong date, there not exist a product.");
+            response.sendRedirect(pathContext + "/InspectionSelectPage");
+            return;
+        }
+        else{
+            List<Questionnaire> todayQuestionnaireList = new ArrayList<>();
+            try {
+                todayQuestionnaireList = product.getQuestionnaires();
+            }catch(PersistenceException e){
+
+            }
+            List<Questionnaire> submittedQuestionnaireList = new ArrayList<>();
+            List<Questionnaire> cancelledQuestionnaireList = new ArrayList<>();
+
+            for(Questionnaire q:todayQuestionnaireList){
+                if(q.getIsCancelled() == 1){
+                    cancelledQuestionnaireList.add(q);
+                }
+                else{
+                    submittedQuestionnaireList.add(q);
+                }
+            }
+            request.getSession().setAttribute("date", date);
+            request.getSession().setAttribute("submittedQuestionnaireList", submittedQuestionnaireList);
+            request.getSession().setAttribute("cancelledQuestionnaireList", cancelledQuestionnaireList);
+            request.getSession().setAttribute("product", product);
+            response.sendRedirect(pathContext + "/InspectionShowPage");
+        }
+
+
+
+//
+//        if (submittedQuestionnaireList.isEmpty() && cancelledQuestionnaireList.isEmpty()){
+//            request.getSession().setAttribute("errorMessage", "No value");
+//            response.sendRedirect(pathContext + "/InspectionPage");
+//            return;
+//        }
+
+
+
+    }
+}
