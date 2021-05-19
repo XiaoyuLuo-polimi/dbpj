99e23c530f771658e070741c43fd5ee0d5ab851a
Xiaoyu Luo <xy.law@outlook.com>
2021-05-19 14:45:37 +0200

    FIX Inspection BUG

--- GamifiedEJB/ejbModule/it/polimi/db2/services/ProductService.java
+++ GamifiedEJB/ejbModule/it/polimi/db2/services/ProductService.java
@@ -19,25 +19,10 @@ public class ProductService {
     }
     public void setNewProductAfterYesterday(String name , int adminId, byte[] imagePath, LocalDate date) throws InvalidInsert, DuplicateInsertion {
         Product product = new Product();
-//        //设置日期格式
-//        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd");
-//        //获取string类型日期
-//        String today=dateFormat.format(date);
-        try{
-            product = em.createNamedQuery("product.getProdByDate", Product.class).setParameter(1, date).getSingleResult();
-        }catch (NoResultException e){
-            product = null;
-        }
-
-        if(product != null){
-            throw new DuplicateInsertion("Today already exist product");
-        }
-        else{
-            product.setProductDate(date);
-            product.setAdminId(adminId);
-            product.setName(name);
-            product.setImage(imagePath);
-
+        product.setProductDate(date);
+        product.setAdminId(adminId);
+        product.setName(name);
+        product.setImage(imagePath);
             try{
                 this.em.persist(product);
                 this.em.flush();
@@ -46,15 +31,10 @@ public class ProductService {
                     throw new InvalidInsert("You cannot insert a POD preceding the current day");
                 }
 
-            }
-
-
-
         }
     }
     public int isExistProductInThatDate(LocalDate date) throws NoResultException{
         Product product = null;
-
         try {
             product = em.createNamedQuery("product.getProdByDate", Product.class).setParameter(1, date).getSingleResult();
         }catch (NoResultException e){
@@ -99,7 +79,6 @@ public class ProductService {
 
     public Product getTodayProduct() throws NoResultException{
         Product product = null;
-
         LocalDate date  = LocalDate.now();
         try {
             product = em.createNamedQuery("product.getProdByDate", Product.class).setParameter(1, date).getSingleResult();
